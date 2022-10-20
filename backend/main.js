259e0cd69575edf1a11af53e78528776d3cb4b55
Abdelrahman Ali <abdelrahmanalisalem@gmail.com>
2022-10-20 21:21:28 +0200

    created backend server and installed packages

---
+++ backend/main.js
@@ -0,0 +1,30 @@
+// External variables
+const express = require("express");
+const mongoose = require('mongoose');
+// THIS IS WRONG NEVER DO THAT !! Only for the task we put the DB Link here!! NEVER DO THAAAT AGAIN !!
+//Check db connection links in README file
+const MongoURI = 'mongodb+srv://admin:admin@cluster0.vm6qaas.mongodb.net/test' ;
+
+
+//App variables
+const app = express();
+const port = process.env.PORT || "3030";
+// const user = require('./Models/User');
+// #Importing the userController
+
+
+// configurations
+// Mongo DB
+mongoose.connect(MongoURI)
+.then(()=>{
+  console.log("MongoDB is now connected!")
+// Starting server
+ app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+  })
+})
+.catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello World")
+})
