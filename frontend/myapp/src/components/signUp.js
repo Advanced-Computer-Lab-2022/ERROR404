2c46e310f77dadac07bea34476d388c3b5a756d9
Abdelrahman Ali <abdelrahmanalisalem@gmail.com>
2022-12-25 22:51:36 +0200

    commit to rebase

--- frontend/myapp/src/components/signUp.js
+++ frontend/myapp/src/components/signUp.js
@@ -9,7 +9,8 @@ import {
   InputNumber,
   Select,
   message,
-  Space
+  Space,
+  Checkbox,
 } from "antd";
 const { Option } = Select;
 
@@ -32,16 +33,8 @@ const SignUp = () => {
     const password = event.password;
     const gender = event.gender;
     const age = event.age;
-       
-    await signUp(
-      firstname,
-      lastname,
-      email,
-      username,
-      password,
-      gender,
-      age
-    );
+
+    await signUp(firstname, lastname, email, username, password, gender, age);
   };
 
   const signUp = async (
@@ -51,8 +44,7 @@ const SignUp = () => {
     username,
     password,
     gender,
-    age,
-    
+    age
   ) => {
     const requestBody = {
       firstname: firstname,
@@ -94,11 +86,11 @@ const SignUp = () => {
           rules={[
             {
               required: true,
-              message: 'Please enter your name',
+              message: "Please enter your name",
             },
           ]}
         >
-          <Input/>
+          <Input />
         </Form.Item>
 
         <Form.Item
@@ -107,29 +99,29 @@ const SignUp = () => {
           rules={[
             {
               required: true,
-              message: 'Please enter your last name',
+              message: "Please enter your last name",
             },
           ]}
         >
-          <Input/>
+          <Input />
         </Form.Item>
 
         <Form.Item
-        label="E-mail"
-        name="email"       
-        rules={[
-          {
-            type: 'email',
-            message: 'The input is not valid E-mail!',
-          },
-          {
-            required: true,
-            message: 'Please enter your E-mail!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+          label="E-mail"
+          name="email"
+          rules={[
+            {
+              type: "email",
+              message: "The input is not valid E-mail!",
+            },
+            {
+              required: true,
+              message: "Please enter your E-mail!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
         <Form.Item
           label="Username"
@@ -137,81 +129,85 @@ const SignUp = () => {
           rules={[
             {
               required: true,
-              message: 'Please enter a username',
+              message: "Please enter a username",
             },
           ]}
         >
-          <Input/>
+          <Input />
         </Form.Item>
 
         <Form.Item
-        name="password"
-        label="Password"
-        rules={[
-          {
-            required: true,
-            message: 'Please enter your password!',
-            min:5,
-          },
-        ]}
-        hasFeedback
-      >
-        <Input.Password />
-      </Form.Item>
-
-        <Form.Item
-        name="confirm"
-        label="Confirm Password"
-        dependencies={['password']}
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: 'Please confirm your password!',
-            min:5,
-          },
-          ({ getFieldValue }) => ({
-            validator(_, value) {
-              if (!value || getFieldValue('password') === value) {
-                return Promise.resolve();
-              }
-              return Promise.reject(new Error('The two passwords that you entered do not match!'));
+          name="password"
+          label="Password"
+          rules={[
+            {
+              required: true,
+              message: "Please enter your password!",
+              min: 5,
             },
-          }),
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
-        
-      
+          ]}
+          hasFeedback
+        >
+          <Input.Password />
+        </Form.Item>
 
-      <Space style={{
-            display: 'flex',
-            marginBottom: 8,
-                    }}
-            //align="baseline"
-            >
         <Form.Item
-        label="Gender"
-        name="gender"
-        rules={[
-          {
-            required: true,
-            message: 'Please select gender!',
-          },
-        ]}
-      >
-        <Select placeholder="Select your gender" style={{width: '100%'}}>
-          <Option value="male">Male</Option>
-          <Option value="female">Female</Option>
-          <Option value="other">Other</Option>
-        </Select>
-      </Form.Item>
+          name="confirm"
+          label="Confirm Password"
+          dependencies={["password"]}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: "Please confirm your password!",
+              min: 5,
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  new Error("The two passwords that you entered do not match!")
+                );
+              },
+            }),
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-        <Form.Item  label= "Age" name="age" >
-          <InputNumber style={{width:'100%'}} />
-        </Form.Item >
-      </Space>
+        <Space
+          style={{
+            display: "flex",
+            marginBottom: 8,
+          }}
+          //align="baseline"
+        >
+          <Form.Item
+            label="Gender"
+            name="gender"
+            rules={[
+              {
+                required: true,
+                message: "Please select gender!",
+              },
+            ]}
+          >
+            <Select placeholder="Select your gender" style={{ width: "100%" }}>
+              <Option value="male">Male</Option>
+              <Option value="female">Female</Option>
+              <Option value="other">Other</Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item label="Age" name="age">
+            <InputNumber style={{ width: "100%" }} />
+          </Form.Item>
+        </Space>
+        <Checkbox>
+          Accept <a href="">Terms of Services</a> and <a href="">Policy</a>
+        </Checkbox>
 
         <Form.Item>
           <Button type="primary" htmlType="submit">
