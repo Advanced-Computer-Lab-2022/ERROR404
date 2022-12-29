45b3b3e866d5eb0fb600e46ddd44b5b805e9070c
Abdelrahman Ali <abdelrahmanalisalem@gmail.com>
2022-12-29 03:47:51 +0200

    Update subtitles.js

--- frontend/myapp/src/components/subtitles.js
+++ frontend/myapp/src/components/subtitles.js
@@ -1,29 +1,27 @@
-import { Avatar, List } from 'antd';
-import React, { useState, useEffect } from 'react';
+import { Avatar, List } from "antd";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { name } from 'nodeman/lib/mustache';
-
 const data = [
-    {
-      title: 'Ant Design Title 1',
-    },
-    {
-      title: 'Ant Design Title 2',
-    },
-    {
-      title: 'Ant Design Title 3',
-    },
-    {
-      title: 'Ant Design Title 4',
-    },
+  {
+    title: "Ant Design Title 1",
+  },
+  {
+    title: "Ant Design Title 2",
+  },
+  {
+    title: "Ant Design Title 3",
+  },
+  {
+    title: "Ant Design Title 4",
+  },
 ];
 
 const Subtitles = () => {
   const [courseId, setCourseId] = useState("");
-  const [courseSubtitles, setCourseSubtitles]= useState([]);
-  const [courseSubName, setCourseSubName]=useState("")
-  const [courseDescription, setCourseDescription]= useState("");
-  const [courseVideo, setCourseVideo]= useState("");
+  const [courseSubtitles, setCourseSubtitles] = useState([]);
+  const [courseSubName, setCourseSubName] = useState("");
+  const [courseDescription, setCourseDescription] = useState("");
+  const [courseVideo, setCourseVideo] = useState("");
 
   useEffect(() => {
     const idSearch = window.location.search;
@@ -45,19 +43,19 @@ const Subtitles = () => {
         console.log(err);
       });
   }, []);
-  return(
+  return (
     <List
-    itemLayout="horizontal"
-    dataSource={data}
-    renderItem={(item) => (
-      <List.Item>
-        <List.Item.Meta
-          title={<a href="https://ant.design">{item.title}</a>}
-          description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-        />
-      </List.Item>
-    )}
-  />
-    );
+      itemLayout="horizontal"
+      dataSource={data}
+      renderItem={(item) => (
+        <List.Item>
+          <List.Item.Meta
+            title={<a href="https://ant.design">{item.title}</a>}
+            description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+          />
+        </List.Item>
+      )}
+    />
+  );
 };
 export default Subtitles;
